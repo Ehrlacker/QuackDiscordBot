6bae0eca9187e50c12065efd6e0a2ad640d8ac8c
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2023-04-20 17:38:13 -0400

    Add random joke slash command. Fix README. Add ES6 module import to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import * as dotenv from 'dotenv'
-import { Client, IntentsBitField } from 'discord.js'
-import axios from "axios"
+const dotenv = require("dotenv")
+const { Client, IntentsBitField } = require("discord.js")
+const axios = require("axios")
 
 dotenv.config()
 
@@ -18,24 +18,41 @@ client.on("ready", (c) => {
   console.log(`Logged in as ${c.user.tag}!`)
 })
 
-
-
 client.on("ready", async () => {
   const command = await client.application.commands.create({
     name: "joke",
-    description: "Provide a joke",
+    description: "Provide a coding joke",
     defaultPermission: false,
   })
   console.log(`Created command ${command.name}`)
 })
 
-export const handleInteraction = async (interaction) => {
+client.on("ready", async () => {
+  const command = await client.application.commands.create({
+    name: "random",
+    description: "Provide a random joke",
+    defaultPermission: false,
+  })
+  console.log(`Created command ${command.name}`)
+})
+
+const handleInteraction = async (interaction) => {
   if (!interaction.isCommand()) return
 
   if (interaction.commandName === "joke") {
-    const joke = await getRandomJoke()
+    const joke = await getRandomJoke(
+      "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    )
     await interaction.reply({
       content: joke,
+      ephemeral: true,
+    })
+  } else if (interaction.commandName === "random") {
+    const random = await getRandomJoke(
+      "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    )
+    await interaction.reply({
+      content: random,
       ephemeral: true,
     })
   }
@@ -43,10 +60,9 @@ export const handleInteraction = async (interaction) => {
 
 client.on("interactionCreate", handleInteraction)
 
-
-async function getRandomJoke() {
+async function getRandomJoke(api) {
   try {
-    const { data } = await axios.get('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
+    const { data } = await axios.get(api)
     if (data.error === true) {
       throw new Error(`Unexpected error`)
     }
@@ -59,3 +75,4 @@ async function getRandomJoke() {
 
 client.login(process.env.TOKEN)
 
+module.exports = { handleInteraction }
